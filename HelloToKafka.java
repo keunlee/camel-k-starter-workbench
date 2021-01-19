2edde13523e413c21f5d6a79b9e87ea7e90a8ad2
hershey <hershey@hershey.lan>
2021-01-19 02:59:54 -0600

    initial commit

---
+++ HelloToKafka.java
@@ -0,0 +1,23 @@
+// camel-k: language=java property-file=application.properties
+// camel-k: dependency=github:keunlee:camel-k-starter-workbench
+
+import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.redhat.integration.common.TestClass;
+
+public class HelloToKafka extends RouteBuilder {
+    private static final Logger LOG = LoggerFactory.getLogger(HelloToKafka.class);
+
+    public void configure() throws Exception {
+
+        TestClass test = new TestClass();
+
+        from("timer:refresh?period=5000&fixedRate=true")
+        .setBody()
+        .simple("Hello World ${header.firedTime}")
+        .log("${body}")
+        .to("kafka:sample-data?brokers={{kafka.bootstrap.address}}");
+    }
+}
