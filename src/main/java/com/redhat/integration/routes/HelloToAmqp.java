51d70db7e1177f57d0257fbcc817a9ece539d063
hershey <hershey@hershey.lan>
2021-01-21 13:51:51 -0600

    restructuring

---
+++ src/main/java/com/redhat/integration/routes/HelloToAmqp.java
@@ -0,0 +1,31 @@
+package com.redhat.integration.routes;
+
+import org.apache.camel.BindToRegistry;
+import org.apache.camel.PropertyInject;
+import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.apache.qpid.jms.JmsConnectionFactory;
+
+import com.redhat.integration.common.TestClass;
+
+public class HelloToAmqp extends RouteBuilder {
+    private static final Logger LOG = LoggerFactory.getLogger(HelloToAmqp.class);
+
+    @PropertyInject("messaging.broker.url.amqp")
+    String messagingBrokerUrl;
+
+    @BindToRegistry
+    public JmsConnectionFactory connectionFactory() throws Exception {
+        return new JmsConnectionFactory(messagingBrokerUrl);
+    }
+
+    @Override
+    public void configure() throws Exception {
+        from("timer:refresh?period=5000&fixedRate=true")
+        .setBody()
+        .simple("Hello World ${header.firedTime}")
+        .log("${body}")
+        .to("amqp:topic:example?exchangePattern=InOnly&connectionFactory=connectionFactory");
+    }
+}
