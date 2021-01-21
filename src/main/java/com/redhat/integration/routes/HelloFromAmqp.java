c8d918a9ad409fe7c1643e17b9bb1329a9c7db72
keunlee <kelee@redhat.com>
2021-01-21 13:51:51 -0600

    restructuring

---
+++ src/main/java/com/redhat/integration/routes/HelloFromAmqp.java
@@ -0,0 +1,26 @@
+package com.redhat.integration.routes;
+
+import org.apache.camel.BindToRegistry;
+import org.apache.camel.PropertyInject;
+import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.apache.qpid.jms.JmsConnectionFactory;
+
+public class HelloFromAmqp extends RouteBuilder {
+    private static final Logger LOG = LoggerFactory.getLogger(HelloFromAmqp.class);
+
+    @PropertyInject("messaging.broker.url.amqp")
+    String messagingBrokerUrl;
+
+    @BindToRegistry
+    public JmsConnectionFactory connectionFactory() throws Exception {
+        return new JmsConnectionFactory(messagingBrokerUrl);
+    }
+
+    @Override
+    public void configure() throws Exception {
+        from("amqp:topic:example?exchangePattern=InOnly&connectionFactory=connectionFactory")
+        .log("${body}");
+    }
+}
