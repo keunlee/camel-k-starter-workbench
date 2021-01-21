fd9ccf3b4426a16c5a90f15d3bfa0e9cdc1665ae
keunlee <kelee@redhat.com>
2021-01-21 14:43:55 -0600

    testing accessible pojos as dependencies

--- src/main/java/com/redhat/integration/routes/HelloToAmqp.java
+++ src/main/java/com/redhat/integration/routes/HelloToAmqp.java
@@ -23,7 +23,7 @@ public class HelloToAmqp extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         AnotherTestClass t = new AnotherTestClass();
-        t.setValue("HI");
+        t.setValue("HI THERE");
 
         from("timer:refresh?period=5000&fixedRate=true")
         .setBody()
