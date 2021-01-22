00486af67544cf0c5a081273beadea34daf435a8
keunlee <kelee@redhat.com>
2021-01-22 01:02:24 -0600

    testing pojos via jitpack

--- HelloFromAmqp.java
+++ HelloFromAmqp.java
@@ -5,6 +5,9 @@ import org.apache.camel.PropertyInject;
 import org.apache.camel.builder.RouteBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import model.src.main.java.com.redhat.integration.common.TestClass001;
+
 import org.apache.qpid.jms.JmsConnectionFactory;
 
 public class HelloFromAmqp extends RouteBuilder {
@@ -20,6 +23,9 @@ public class HelloFromAmqp extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+        TestClass001 t = new TestClass001();
+        t.setValue("setting values");
+
         from("amqp:topic:example?exchangePattern=InOnly&connectionFactory=connectionFactory")
         .log("${body}");
     }
---
+++ model/src/main/java/com/redhat/integration/common/TestClass002.java
@@ -0,0 +1,13 @@
+package model.src.main.java.com.redhat.integration.common;
+
+public class TestClass002 {
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }   
+}
